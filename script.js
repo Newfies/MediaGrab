592becb90c5c6dabe3403e0b7e2aabef7b320038
Newfie <157941859+Newfies@users.noreply.github.com>
2025-05-16 09:02:23 -0500

    Add files via upload

--- script.js
+++ script.js
@@ -36,10 +36,12 @@ document.addEventListener("DOMContentLoaded", function(){
 
             if (urlInput.value.includes('youtu.be/')){
                 // Its a YouTube Shortened URL!
+                
             }
 
             else {
                 // Its an unsupported URL!
+                report.innerText = `Invalid URL Entered`;
             }
         })
     }
@@ -51,5 +53,20 @@ document.addEventListener("DOMContentLoaded", function(){
 
 https://www.googleapis.com/youtube/v3/videos?part=snippet&id={YOUTUBE_VIDEO_ID}&fields=items(id,snippet)&key=AIzaSyAZCjFb4Gjmw6o_mHzsaGucQ86-cA8t9Sw
 https://developers.google.com/youtube/v3/getting-started#calculating-quota-usage
+
+
+
+$.get("https://www.googleapis.com/youtube/v3/videos?part=snippet&id=" + videoId + "&key=AIzaSyAZCjFb4Gjmw6o_mHzsaGucQ86-cA8t9Sw")
+                        .done(function(data) {
+                            if (data.items.length > 0) {
+                                report.innerText = data.items[0].snippet.title;
+                            } else {
+                                report.innerText = `Video not found.`;
+                            }
+                        })
+                        .fail(function(jqXHR, textStatus, errorThrown) {
+                            console.error("API request failed:", textStatus, errorThrown);
+                            report.innerText = `Error retrieving video information: ${errorThrown}`;
+                    });
 
 */
